481efb08d09b242d4347da1ddd7dcf62fc6c1c1c
mayank <meiyanksingh@gmail.com>
2025-03-17 21:41:39 +0530

    loader changed

--- nagp-ecommerce-main/src/utils/apiURL.js
+++ nagp-ecommerce-main/src/utils/apiURL.js
@@ -1,3 +1,3 @@
- export const BASE_URL = 'http://localhost:8080';
-//export const BASE_URL = 'http://15.207.101.99:8080';
+// export const BASE_URL = 'http://localhost:8080';
+export const BASE_URL = 'http://13.235.0.57:8080';
 
